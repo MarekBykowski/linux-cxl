84965f8282952cad51692930ff82416ee482519d
Marek Bykowski <marek.bykowski@gmail.com>
2023-11-22 17:27:01 +0000

    add debug to drivers/char/mem.c

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -9,6 +9,9 @@
  *  Shared /dev/zero mmapping support, Feb 2000, Kanoj Sarcar <kanoj@sgi.com>
  */
 
+
+#define DEBUG
+
 #include <linux/mm.h>
 #include <linux/miscdevice.h>
 #include <linux/slab.h>
